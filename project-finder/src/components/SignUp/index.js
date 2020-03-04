9b3262d39d9fd81ce9b5dd96337e12d3dda1df2c
wongJonathan <wong.jonathan@live.com>
2020-03-04 18:05:44 -0500

    Can create user accounts

--- project-finder/src/components/SignUp/index.js
+++ project-finder/src/components/SignUp/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+
+import SignUpForm from "./SignUpForm";
+
+
 const SignUpPage = () => (
   <div>
     <h1>Sign Up</h1>
+    <SignUpForm />
   </div>
 );
 export default SignUpPage;
