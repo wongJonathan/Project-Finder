3d67c75db7330c2ede57c8e81dc9c90672c6ba87
wongJonathan <wong.jonathan@live.com>
2020-03-12 16:25:30 -0400

    Started on password change

---
+++ project-finder/src/components/PasswordChange/__test__/index.test.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+
+import PasswordChangeForm from '../index';
+import Firebase from '../../Firebase/firebase';
+import { FirebaseContext } from '../../Firebase';
+
+jest.mock('../../Firebase/firebase');
+
+describe('Password Change Form', () => {
+  let passwordChangeForm;
+  let mockFirebase;
+
+  beforeEach(() => {
+    Firebase.mockClear();
+    mockFirebase = new Firebase();
+    passwordChangeForm = (
+      <FirebaseContext.Provider value={mockFirebase}>
+        <PasswordChangeForm />
+      </FirebaseContext.Provider>
+    );
+  });
+
+  it('should render', () => {
+    const { getByPlaceholderText } = render(passwordChangeForm);
+
+    expect(getByPlaceholderText('New Password')).not.toBeNull();
+    expect(getByPlaceholderText('Confirm New Password')).not.toBeNull();
+  });
+});
