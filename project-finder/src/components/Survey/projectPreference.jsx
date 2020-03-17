950293e98db5987574d3751d5f20a8466c9e9477
wongJonathan <wong.jonathan@live.com>
2020-03-16 22:01:03 -0400

    Switching over to react-sortable-hoc

--- project-finder/src/components/Survey/projectPreference.jsx
+++ project-finder/src/components/Survey/projectPreference.jsx
@@ -3,6 +3,7 @@ import { Motion, spring } from 'react-motion/lib/react-motion';
 
 
 const reinsert = (arr, from, to) => {
+  console.log(arr, from, to);
   const output = arr.slice(0);
   const val = output[from];
   output.splice(from, 1);
@@ -14,6 +15,7 @@ const clamp = (n, min, max) => Math.max(Math.min(n, max), min);
 
 const springConfig = { stiffness: 300, damping: 50 };
 const CATEGORY_NAMES = ['Web', 'Android App', 'iOS App', 'Voice'];
+const divHeight = 100;
 
 const ProjectPreference = () => {
   const [preferenceList, setList] = useState(CATEGORY_NAMES);
@@ -23,46 +25,45 @@ const ProjectPreference = () => {
   const [originalPosOfLastPressed, setLastPressed] = useState(0);
 
   const handleMouseDown = (pos, pressY, { pageY }) => {
-    console.log('mouseDown');
+    console.log('Mouse down:', pressY, pageY);
     setDeltaY(pageY - pressY);
     setMouseY(pressY);
     setPressed(true);
     setLastPressed(pos);
   };
 
-  const handleMouseMove = ({ pageY }) => {
-    console.log('mouse move');
+  const handleMouseMove = useCallback(({ pageY }) => {
     if (isPressed) {
       const newMouseY = pageY - topDeltaY;
-      const currentRow = clamp(Math.round(newMouseY / 100), 0, preferenceList.length - 1);
-      let newOrder = preferenceList;
+      console.log(newMouseY, pageY, topDeltaY);
+      const currentRow = clamp(Math.round(newMouseY / divHeight), 0, preferenceList.length - 1);
+      // console.log(currentRow);
 
       if (currentRow !== originalPosOfLastPressed) {
-        newOrder = reinsert(
+        const newList = reinsert(
           preferenceList,
           originalPosOfLastPressed,
           currentRow,
         );
+        console.log('new list');
+        console.log(newList);
+        setList(newList);
       }
 
       setMouseY(newMouseY);
-      setList(newOrder);
     }
-  };
+  }, [isPressed, originalPosOfLastPressed, topDeltaY, preferenceList]);
 
   const handleTouchStart = (key, pressLocation, e) => {
-    console.log('touch start');
     handleMouseDown(key, pressLocation, e.touches[0]);
   };
 
   const handleTouchMove = useCallback((e) => {
-    console.log('touche move');
     e.preventDefault();
     handleMouseMove(e.touches[0]);
   }, [handleMouseMove]);
 
   const handleMouseUp = () => {
-    console.log('up');
     setPressed(false);
     setDeltaY(0);
   };
@@ -72,21 +73,29 @@ const ProjectPreference = () => {
     window.addEventListener('touchend', handleMouseUp);
     window.addEventListener('mousemove', handleMouseMove);
     window.addEventListener('mouseup', handleMouseUp);
+
+    return function cleanup() {
+      window.removeEventListener('touchmove', handleTouchMove);
+      window.removeEventListener('touchend', handleMouseUp);
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('mouseup', handleMouseUp);
+    };
   }, [handleMouseMove, handleTouchMove]);
 
-  console.log(isPressed, topDeltaY, mouseY, originalPosOfLastPressed);
+  // console.log(isPressed, topDeltaY, mouseY, originalPosOfLastPressed);
+  // console.log(preferenceList);
 
   return (
     <div>
-      {preferenceList.map((name, index) => {
+      {CATEGORY_NAMES.map((name, index) => {
         const style = originalPosOfLastPressed === index && isPressed
           ? {
             scale: spring(1.1, springConfig),
-            y: mouseY,
+            y: mouseY ,
           }
           : {
             scale: spring(1, springConfig),
-            y: spring(index * 100, springConfig),
+            y: spring(preferenceList.indexOf(name) * divHeight, springConfig),
           };
 
         return (
@@ -95,13 +104,15 @@ const ProjectPreference = () => {
               // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
               <div
                 role="list"
-                onMouseDown={(e) => handleMouseDown(index, y, e)}
-                onTouchStart={(e) => handleTouchStart(index, y, e)}
+                onMouseDown={(e) => handleMouseDown(index, preferenceList.indexOf(name) * 100, e)}
+                onTouchStart={(e) => handleTouchStart(index, preferenceList.indexOf(name) * 100, e)}
                 className="demo8-item"
                 style={{
                   transform: `translate3d(0, ${y}px, 0) scale(${scale})`,
                   WebkitTransform: `translate3d(0, ${y}px, 0) scale(${scale})`,
                   zIndex: index === originalPosOfLastPressed ? 99 : index,
+                  borderStyle: 'solid',
+                  height: `${divHeight}px`,
                 }}
               >
                 {name}
