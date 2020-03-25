4523208653d1107f9fab1ecb43691a38f202adcc
wongJonathan <wong.jonathan@live.com>
2020-03-24 20:03:37 -0400

    Finished password forget

--- project-finder/src/components/PasswordForget/index.tsx
+++ project-finder/src/components/PasswordForget/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 
 import PasswordForgetForm from './PasswordForgetForm';
 
 
-const PasswordForgetPage = () => (
+const PasswordForgetPage = (): ReactElement => (
   <div>
     <h1>Password Forget</h1>
     <PasswordForgetForm />
